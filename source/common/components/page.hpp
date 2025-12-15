24001cbca1b2c447a5c7398ab0b247cad10c3e4f
ZiadHF <ziadhfahmy@yahoo.com>
2025-12-15 04:04:46 +0200

    added some new pages, comments, modified attributes in json

--- source/common/components/page.hpp
+++ source/common/components/page.hpp
@@ -1,6 +1,8 @@
 namespace our {
     class PageComponent : public Component {
       public:
+        // Note: This component is pretty much just a marker component and can probably be removed
+        // Has the page been collected by the player
         bool isCollected = false;
 
         static std::string getID() {
